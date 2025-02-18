b2766718ba898fabaf409b4f0111f9fd18a7022b
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-02-18 15:21:26 -0300

    Updated to work over default behaviour

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,140 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
 import logging
+
 _logger = logging.getLogger(__name__)
 class Picking(models.Model):
     _inherit = 'stock.picking'
     
     @api.model_create_multi
     def create(self, vals_list):
-        scheduled_dates = []
-        for vals in vals_list:
-            defaults = self.default_get(['name', 'picking_type_id'])
-            picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id', defaults.get('picking_type_id')))
-            if vals.get('name', '/') == '/' and defaults.get('name', '/') == '/' and vals.get('picking_type_id', defaults.get('picking_type_id')):
-                if picking_type.sequence_id:
-                    _logger.info('Using new sequence')
-                    vals['name'] = picking_type.sequence_id.get_next_char(picking_type.sequence_id.number_next_actual)
-
-            # make sure to write `schedule_date` *after* the `stock.move` creation in
-            # order to get a determinist execution of `_set_scheduled_date`
-            scheduled_dates.append(vals.pop('scheduled_date', False))
-
         pickings = super().create(vals_list)
-
-        for picking, scheduled_date in zip(pickings, scheduled_dates):
-            if scheduled_date:
-                picking.with_context(mail_notrack=True).write({'scheduled_date': scheduled_date})
-        pickings._autoconfirm_picking()
-
-        for picking, vals in zip(pickings, vals_list):
-            # set partner as follower
-            if vals.get('partner_id'):
-                if picking.location_id.usage == 'supplier' or picking.location_dest_id.usage == 'customer':
-                    picking.message_subscribe([vals.get('partner_id')])
-            if vals.get('picking_type_id'):
-                for move in picking.move_ids:
-                    if not move.description_picking:
-                        move.description_picking = move.product_id.with_context(lang=move._get_lang())._get_description(move.picking_id.picking_type_id)
+        if 'picking_type_id' in vals_list:
+            for picking in self:
+                sequence = picking.picking_type_id.sequence_id
+                #_logger.info(f"Revirtiendo secuencia para {picking.name}")
+                sequence.sudo().write({'number_next': sequence.number_next_actual - 1})
         return pickings
     
     def write(self, vals):
-        if vals.get('picking_type_id') and any(picking.state != 'draft' for picking in self):
-            raise UserError(_("Changing the operation type of this record is forbidden at this point."))
-        # set partner as a follower and unfollow old partner
-        if vals.get('partner_id'):
-            for picking in self:
-                if picking.location_id.usage == 'supplier' or picking.location_dest_id.usage == 'customer':
-                    if picking.partner_id:
-                        picking.message_unsubscribe(picking.partner_id.ids)
-                    picking.message_subscribe([vals.get('partner_id')])
-        if vals.get('picking_type_id'):
-            picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id'))
-            for picking in self:
-                if picking.picking_type_id != picking_type:
-                    _logger.info('Updating sequence')
-                    picking.name = picking_type.sequence_id.get_next_char(picking_type.sequence_id.number_next_actual)
         res = super(Picking, self).write(vals)
-        if vals.get('signature'):
+        # Restaurar la secuencia original solo si `picking_type_id` fue cambiado
+        if 'picking_type_id' in vals:
             for picking in self:
-                picking._attach_sign()
-        # Change locations of moves if those of the picking change
-        after_vals = {}
-        if vals.get('location_id'):
-            after_vals['location_id'] = vals['location_id']
-        if vals.get('location_dest_id'):
-            after_vals['location_dest_id'] = vals['location_dest_id']
-        if 'partner_id' in vals:
-            after_vals['partner_id'] = vals['partner_id']
-        if after_vals:
-            self.move_ids.filtered(lambda move: not move.scrapped).write(after_vals)
-        if vals.get('move_ids') or vals.get('move_ids_without_package'):
-            self._autoconfirm_picking()
-
+                sequence = picking.picking_type_id.sequence_id
+                #_logger.info(f"Revirtiendo secuencia para {picking.name}")
+                sequence.sudo().write({'number_next': sequence.number_next_actual - 1})
         return res
     
     def button_validate(self):
-        draft_picking = self.filtered(lambda p: p.state == 'draft')
-        draft_picking.action_confirm()
-
         self.name = self.picking_type_id.sequence_id.next_by_id()
+
+        res = super(Picking, self).button_validate()
         
-        for move in draft_picking.move_ids:
-            if float_is_zero(move.quantity, precision_rounding=move.product_uom.rounding) and\
-               not float_is_zero(move.product_uom_qty, precision_rounding=move.product_uom.rounding):
-                move.quantity = move.product_uom_qty
-
-        # Sanity checks.
-        if not self.env.context.get('skip_sanity_check', False):
-            self._sanity_check()
-        self.message_subscribe([self.env.user.partner_id.id])
-
-        # Run the pre-validation wizards. Processing a pre-validation wizard should work on the
-        # moves and/or the context and never call `_action_done`.
-        if not self.env.context.get('button_validate_picking_ids'):
-            self = self.with_context(button_validate_picking_ids=self.ids)
-        res = self._pre_action_done_hook()
-        if res is not True:
-            return res
-
-        # Call `_action_done`.
-        pickings_not_to_backorder = self.filtered(lambda p: p.picking_type_id.create_backorder == 'never')
-        if self.env.context.get('picking_ids_not_to_backorder'):
-            pickings_not_to_backorder |= self.browse(self.env.context['picking_ids_not_to_backorder']).filtered(
-                lambda p: p.picking_type_id.create_backorder != 'always'
-            )
-        pickings_to_backorder = self - pickings_not_to_backorder
-        pickings_not_to_backorder.with_context(cancel_backorder=True)._action_done()
-        pickings_to_backorder.with_context(cancel_backorder=False)._action_done()
-        report_actions = self._get_autoprint_report_actions()
-        another_action = False
-        if self.user_has_groups('stock.group_reception_report'):
-            pickings_show_report = self.filtered(lambda p: p.picking_type_id.auto_show_reception_report)
-            lines = pickings_show_report.move_ids.filtered(lambda m: m.product_id.type == 'product' and m.state != 'cancel' and m.quantity and not m.move_dest_ids)
-            if lines:
-                # don't show reception report if all already assigned/nothing to assign
-                wh_location_ids = self.env['stock.location']._search([('id', 'child_of', pickings_show_report.picking_type_id.warehouse_id.view_location_id.ids), ('usage', '!=', 'supplier')])
-                if self.env['stock.move'].search([
-                        ('state', 'in', ['confirmed', 'partially_available', 'waiting', 'assigned']),
-                        ('product_qty', '>', 0),
-                        ('location_id', 'in', wh_location_ids),
-                        ('move_orig_ids', '=', False),
-                        ('picking_id', 'not in', pickings_show_report.ids),
-                        ('product_id', 'in', lines.product_id.ids)], limit=1):
-                    action = pickings_show_report.action_view_reception_report()
-                    action['context'] = {'default_picking_ids': pickings_show_report.ids}
-                    if not report_actions:
-                        return action
-                    another_action = action
-        if report_actions:
-            return {
-                'type': 'ir.actions.client',
-                'tag': 'do_multi_print',
-                'params': {
-                    'reports': report_actions,
-                    'anotherAction': another_action,
-                }
-            }
-        return True
+        return res
